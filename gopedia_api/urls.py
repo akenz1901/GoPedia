e858b67b00efb17c8dac259e855550c1d42e9b53
akenz1901 <michael_aka1@icloud.com>
2022-07-17 00:31:22 +0100

    Modified URL

--- gopedia_api/urls.py
+++ gopedia_api/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('student/', views.create_student, name='create student'),
-    path('student/<email>/', views.update_student_purchase, name='Fetch, Remove or Modify single student')
+    path('student/<email>/', views.update_student_purchase, name='Fetch, Remove or Modify a student')
 ]
