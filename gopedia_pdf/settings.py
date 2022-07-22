89e1a4ca15f860ef8f378e611ba79f1e970df8b5
akenz1901 <michael_aka1@icloud.com>
2022-07-22 11:58:18 +0100

    Image url configued

--- gopedia_pdf/settings.py
+++ gopedia_pdf/settings.py
@@ -142,6 +142,10 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
+MEDIA_ROOT = BASE_DIR / 'media'
+
+MEDIA_URL = "/media/"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
